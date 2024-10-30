e17901d42f6ef3e2bed15073402748e76ea8cfcd
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-30 12:31:56 +0600

    Update Full Code Together in main.py

--- Full Code Together in main.py
+++ Full Code Together in main.py
@@ -1,2 +1,10 @@
-import pywhatkit
-pywhatkit.sendwhatmsg('01712799607','Assalamualaikum',12,15)
+import pywhatkit as kit
+kit.sendwhatmsg('01712799607','Assalamualaikum',12,15)
+kit.sendwhats_image('+8801712799607', 'path/to/your/image.jpg', 'Caption for the image')
+kit.sendwhats_image('+8801712799607', 'path/to/your/audiofile.mp3')
+kit.sendwhats_image('+8801712799607', 'path/to/your/image.jpg', 'Caption for the image')
+kit.sendwhats_image('+8801712799607', 'path/to/your/videofile.mp4')
+
+
+
+
